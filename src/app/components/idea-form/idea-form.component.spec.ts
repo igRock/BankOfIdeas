fefb151019a34456da5b7fbea3364918bbd311ff
igRock <topchiy.greg@yandex.ru>
2018-05-02 09:17:55 +0300

    BankOfIdeasVersion2WithUnitTests

--- src/app/components/idea-form/idea-form.component.spec.ts
+++ src/app/components/idea-form/idea-form.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IdeaFormComponent } from './idea-form.component';
 
 describe('IdeaFormComponent', () => {
-  let component: IdeaFormComponent;
+  let ideaFormTest: IdeaFormComponent;
   let fixture: ComponentFixture<IdeaFormComponent>;
 
   beforeEach(async(() => {
@@ -15,11 +15,24 @@ describe('IdeaFormComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IdeaFormComponent);
-    component = fixture.componentInstance;
+    ideaFormTest = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
+    expect(ideaFormTest).toBeTruthy();
+  });
+  
+  describe('onSubmit()', () => {
+    
+    it('1. should clear all the forms', () => {
+    
+      ideaFormTest.onSubmit();
+
+      expect(ideaFormTest.idea.title).toEqual('');
+      expect(ideaFormTest.idea.topic).toEqual('');
+      expect(ideaFormTest.idea.description).toEqual('');
+      expect(ideaFormTest.isAnonymous).toBeFalsy();
+    });
   });
 });
--- src/app/components/idea-item/idea-item.component.spec.ts
+++ src/app/components/idea-item/idea-item.component.spec.ts
@@ -1,25 +1,94 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { LikeService } from '../../services/like.service';
 
 import { IdeaItemComponent } from './idea-item.component';
+import { IdeaItem } from '../../models/idea-item.model';
 
 describe('IdeaItemComponent', () => {
-  let component: IdeaItemComponent;
+  let ideaItemTest: IdeaItemComponent;
   let fixture: ComponentFixture<IdeaItemComponent>;
+  let testIdea : IdeaItem = {
+    email : 'test@test.ru',
+    userName : 'testName',
+    title : 'testTitle',
+    description: 'testDescription',
+    topic : 'testTopic',
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IdeaItemComponent ]
+      declarations: [ IdeaItemComponent ],
+      providers : [ LikeService ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IdeaItemComponent);
-    component = fixture.componentInstance;
+    ideaItemTest = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
+    expect(ideaItemTest).toBeTruthy();
+  });
+
+  describe('editIdea()', () => {
+    it('1. should turn editState on', () => {
+      ideaItemTest.editIdea(event, testIdea);
+      expect(ideaItemTest.editState).toBeTruthy();
+    });
+
+    it('2. should create an ideaItem', () => {
+      ideaItemTest.editIdea(event, testIdea);
+      expect(ideaItemTest.ideaItem.email).toEqual('test@test.ru');
+      expect(ideaItemTest.ideaItem.userName).toEqual('testName');
+      expect(ideaItemTest.ideaItem.title).toEqual('testTitle');
+      expect(ideaItemTest.ideaItem.description).toEqual('testDescription');
+      expect(ideaItemTest.ideaItem.topic).toEqual('testTopic');
+    });
+  });
+
+  describe('updateIdea()', () => {
+    it('1. should turn editState off', () => {
+      ideaItemTest.updateIdea(testIdea);
+      expect(ideaItemTest.editState).toBeFalsy();
+    });
+
+    it('2. should make ideaToEdit to be Null', () => {
+      ideaItemTest.updateIdea(testIdea);
+      expect(ideaItemTest.ideaToEdit).toBeNull();
+    });
+
+    it('3. should update an ideaItem properties', () => {
+      ideaItemTest.updateIdea(testIdea);
+      expect(ideaItemTest.ideaItem.title).toEqual('testTitle');
+      expect(ideaItemTest.ideaItem.description).toEqual('testDescription');
+      expect(ideaItemTest.ideaItem.topic).toEqual('testTopic');
+    });
+  });
+
+  describe('deleteIdea()', () => {
+    it('1. should turn editState off', () => {
+      ideaItemTest.deleteIdea(event, testIdea);
+      expect(ideaItemTest.editState).toBeFalsy();
+    });
+
+    it('2. should make ideaToEdit to be Null', () => {
+      ideaItemTest.deleteIdea(event, testIdea);
+      expect(ideaItemTest.ideaToEdit).toBeNull();
+    });
+  });
+
+  describe('clearState()', () => {
+    it('1. should turn editState off', () => {
+      ideaItemTest.clearState();
+      expect(ideaItemTest.editState).toBeFalsy();
+    });
+
+    it('2. should make ideaToEdit to be Null', () => {
+      ideaItemTest.clearState();
+      expect(ideaItemTest.ideaToEdit).toBeNull();
+    });
   });
 });
